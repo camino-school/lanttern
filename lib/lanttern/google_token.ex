10e34d5aae9b3a2ad89cee56392e919f99b301b4
Eric Endo <eric.endo@caminoeducation.com>
2023-08-10 01:07:28 -0300

    fix: adjusted env variable

--- lib/lanttern/google_token.ex
+++ lib/lanttern/google_token.ex
@@ -1,16 +1,18 @@
 defmodule Lanttern.GoogleToken do
   use Joken.Config
 
-  @client_id Application.compile_env(:lanttern, LantternWeb.UserAuth)
-             |> Keyword.get(:google_client_id)
   @google_iss_1 "https://accounts.google.com"
   @google_iss_2 "accounts.google.com"
 
   add_hook(JokenJwks, strategy: Lanttern.GoogleTokenStrategy)
 
   def token_config do
+    client_id =
+      Application.get_env(:lanttern, LantternWeb.UserAuth)
+      |> Keyword.get(:google_client_id)
+
     %{}
-    |> add_claim("aud", fn -> @client_id end, &(&1 == @client_id))
+    |> add_claim("aud", fn -> client_id end, &(&1 == client_id))
     |> add_claim("iss", fn -> @google_iss_1 end, &(&1 == @google_iss_1 or &1 == @google_iss_2))
     |> add_claim("exp", fn -> generate_exp() end, &(&1 > Timex.now() |> Timex.to_unix()))
   end
