4acd3bd353e2e663c32360e3cd4824e9aecb3cbf
Eric Endo <eric.endo@caminoeducation.com>
2023-10-23 14:50:04 -0300

    chore: added "seeds" to static paths

--- lib/lanttern_web.ex
+++ lib/lanttern_web.ex
@@ -17,7 +17,7 @@ defmodule LantternWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images seeds favicon.ico robots.txt)
 
   def router do
     quote do
