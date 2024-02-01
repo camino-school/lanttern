7caa83c88ee315358e1460edfa13d75e13969e70
endoooo <eric.ehe@gmail.com>
2024-02-01 09:39:54 -0300

    chore: added url host to endpoint config

--- config/runtime.exs
+++ config/runtime.exs
@@ -17,7 +17,9 @@ import Config
 # Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
 # script that automatically sets the env var above.
 if System.get_env("PHX_SERVER") do
-  config :lanttern, LantternWeb.Endpoint, server: true
+  config :lanttern, LantternWeb.Endpoint,
+    server: true,
+    url: [host: System.get_env("URL_HOST")]
 end
 
 if config_env() == :prod do
