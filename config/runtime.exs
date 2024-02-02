260943c0cd2573a0ab65dc8966a74b78022eb6a5
endoooo <eric.ehe@gmail.com>
2024-02-02 12:51:14 -0300

    chore: host config

--- config/runtime.exs
+++ config/runtime.exs
@@ -17,10 +17,7 @@ import Config
 # Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
 # script that automatically sets the env var above.
 if System.get_env("PHX_SERVER") do
-  config :lanttern, LantternWeb.Endpoint,
-    server: true,
-    url: [host: System.get_env("URL_HOST")],
-    check_origin: true
+  config :lanttern, LantternWeb.Endpoint, server: true
 end
 
 if config_env() == :prod do
