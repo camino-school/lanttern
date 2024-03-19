051334bf0bea9730eb39f207d5a0357dea6bd6e1
endoooo <eric.ehe@gmail.com>
2024-03-19 10:23:14 -0300

    chore: created base function for cycle grades calculation
    - adjusted `calculate_student_grade/3` spec. using separated, ordered params instead of keyword list as opts
    
    - created `calculate_student_grades/2` for calculating all student grades for a given cycle

--- lib/lanttern/grades_reports.ex
+++ lib/lanttern/grades_reports.ex
@@ -11,6 +11,7 @@ defmodule Lanttern.GradesReports do
   alias Lanttern.Grading
   alias Lanttern.Grading.OrdinalValue
   alias Lanttern.Reporting.GradesReportCycle
+  alias Lanttern.Reporting.GradesReportSubject
 
   @doc """
   Returns the list of student_grade_report_entries.
@@ -114,72 +115,163 @@ defmodule Lanttern.GradesReports do
 
   @doc """
   Calculate student grade based for given grades report cycle and subject.
-
-  ## Expected opts:
-
-  - `:student_id`
-  - `:grades_report_cycle_id`
-  - `:grades_report_subject_id`
   """
-  @spec calculate_student_grade(Keyword.t()) ::
-          {:ok, StudentGradeReportEntry.t()} | {:error, Ecto.Changeset.t()} | nil
-  def calculate_student_grade(opts \\ []) do
-    with student_id when not is_nil(student_id) <- Keyword.get(opts, :student_id),
-         grades_report_cycle_id when not is_nil(grades_report_cycle_id) <-
-           Keyword.get(opts, :grades_report_cycle_id),
-         grades_report_subject_id when not is_nil(grades_report_subject_id) <-
-           Keyword.get(opts, :grades_report_subject_id) do
-      entries_grade_components_and_assessment_points =
-        from(
-          e in AssessmentPointEntry,
-          join: s in assoc(e, :scale),
-          left_join: ov in assoc(e, :ordinal_value),
-          join: ap in assoc(e, :assessment_point),
-          join: gc in assoc(ap, :grade_components),
-          join: rc in assoc(gc, :report_card),
-          join: gr in assoc(rc, :grades_report),
-          join: grc in assoc(gr, :grades_report_cycles),
-          join: grs in assoc(gr, :grades_report_subjects),
-          where: e.student_id == ^student_id,
-          where: grc.id == ^grades_report_cycle_id,
-          where: grs.id == ^grades_report_subject_id,
-          preload: [ordinal_value: ov, scale: s],
-          select: {e, gc}
-        )
-        |> Repo.all()
+  @spec calculate_student_grade(
+          student_id :: integer(),
+          grades_report_cycle_id :: integer(),
+          grades_report_subject_id :: integer()
+        ) ::
+          {:ok, StudentGradeReportEntry.t()} | {:error, Ecto.Changeset.t()}
+  def calculate_student_grade(student_id, grades_report_cycle_id, grades_report_subject_id) do
+    entries_grade_components_and_assessment_points =
+      from(
+        e in AssessmentPointEntry,
+        join: s in assoc(e, :scale),
+        left_join: ov in assoc(e, :ordinal_value),
+        join: ap in assoc(e, :assessment_point),
+        join: gc in assoc(ap, :grade_components),
+        join: rc in assoc(gc, :report_card),
+        join: gr in assoc(rc, :grades_report),
+        join: grc in assoc(gr, :grades_report_cycles),
+        join: grs in GradesReportSubject,
+        on: grs.grades_report_id == gr.id and grs.subject_id == gc.subject_id,
+        where: e.student_id == ^student_id,
+        where: grc.id == ^grades_report_cycle_id,
+        where: grs.id == ^grades_report_subject_id,
+        preload: [ordinal_value: ov, scale: s],
+        select: {e, gc}
+      )
+      |> Repo.all()
 
-      # calculate the weighted average
-      {sumprod, sumweight} =
-        entries_grade_components_and_assessment_points
-        |> Enum.reduce({0, 0}, fn {e, gc}, {sumprod, sumweight} ->
-          {get_normalized_value_from_entry(e) * gc.weight + sumprod, gc.weight + sumweight}
-        end)
+    {scale, grades_report} =
+      get_scale_and_grades_report_from_grades_report_cycle(grades_report_cycle_id)
 
-      normalized_avg = Float.round(sumprod / sumweight, 5)
+    # calculate the weighted average
+    {sumprod, sumweight} =
+      entries_grade_components_and_assessment_points
+      |> Enum.reduce({0, 0}, fn {e, gc}, {sumprod, sumweight} ->
+        {get_normalized_value_from_entry(e) * gc.weight + sumprod, gc.weight + sumweight}
+      end)
 
-      {scale, grades_report} =
-        get_scale_and_grades_report_from_grades_report_cycle(grades_report_cycle_id)
+    normalized_avg = Float.round(sumprod / sumweight, 5)
+    scale_value = Grading.convert_normalized_value_to_scale_value(normalized_avg, scale)
 
-      scale_value = Grading.convert_normalized_value_to_scale_value(normalized_avg, scale)
+    # setup student grade report entry attrs and create
+    case scale_value do
+      %OrdinalValue{} = ordinal_value ->
+        %{ordinal_value_id: ordinal_value.id}
 
-      # setup student grade report entry attrs and create
-      case scale_value do
-        %OrdinalValue{} = ordinal_value ->
-          %{ordinal_value_id: ordinal_value.id}
+      score ->
+        %{score: score}
+    end
+    |> Enum.into(%{
+      student_id: student_id,
+      normalized_value: normalized_avg,
+      grades_report_id: grades_report.id,
+      grades_report_cycle_id: grades_report_cycle_id,
+      grades_report_subject_id: grades_report_subject_id
+    })
+    |> create_student_grade_report_entry()
+  end
 
-        score ->
-          %{score: score}
-      end
-      |> Enum.into(%{
+  @doc """
+  Calculate student grades for all subjects in given grades report cycle.
+  """
+  @spec calculate_student_grades(
+          student_id :: integer(),
+          grades_report_cycle_id :: integer()
+        ) ::
+          {:ok, [StudentGradeReportEntry.t()]} | {:error, Ecto.Changeset.t()}
+  def calculate_student_grades(student_id, grades_report_cycle_id) do
+    entries_grade_components_and_grades_report_subject =
+      from(
+        e in AssessmentPointEntry,
+        join: s in assoc(e, :scale),
+        left_join: ov in assoc(e, :ordinal_value),
+        join: ap in assoc(e, :assessment_point),
+        join: gc in assoc(ap, :grade_components),
+        join: rc in assoc(gc, :report_card),
+        join: gr in assoc(rc, :grades_report),
+        join: grc in assoc(gr, :grades_report_cycles),
+        join: grs in GradesReportSubject,
+        on: grs.grades_report_id == gr.id and grs.subject_id == gc.subject_id,
+        where: e.student_id == ^student_id,
+        where: grc.id == ^grades_report_cycle_id,
+        preload: [ordinal_value: ov, scale: s],
+        select: {e, gc, grs}
+      )
+      |> Repo.all()
+
+    {scale, grades_report} =
+      get_scale_and_grades_report_from_grades_report_cycle(grades_report_cycle_id)
+
+    # group entries by grades report subject,
+    # calculate normalized values for each group,
+    # and get scale value conversion
+    grades_report_subjects_n_values_and_scale_values =
+      entries_grade_components_and_grades_report_subject
+      |> Enum.group_by(fn {_e, _gc, grs} -> grs.id end)
+      |> Enum.map(fn {grs_id, e_gc_grs_list} ->
+        # calculate the weighted average
+        {sumprod, sumweight} =
+          e_gc_grs_list
+          |> Enum.reduce({0, 0}, fn {e, gc, _grs}, {sumprod, sumweight} ->
+            {get_normalized_value_from_entry(e) * gc.weight + sumprod, gc.weight + sumweight}
+          end)
+
+        normalized_avg = Float.round(sumprod / sumweight, 5)
+        scale_value = Grading.convert_normalized_value_to_scale_value(normalized_avg, scale)
+
+        {grs_id, normalized_avg, scale_value}
+      end)
+
+    Repo.transaction(fn ->
+      %{
         student_id: student_id,
-        normalized_value: normalized_avg,
         grades_report_id: grades_report.id,
-        grades_report_cycle_id: grades_report_cycle_id,
-        grades_report_subject_id: grades_report_subject_id
-      })
-      |> create_student_grade_report_entry()
+        grades_report_cycle_id: grades_report_cycle_id
+      }
+      |> do_create_student_grade_report_entry(grades_report_subjects_n_values_and_scale_values)
+    end)
+  end
+
+  defp do_create_student_grade_report_entry(base_map, grsid_nv_sv_list, entries \\ [])
+
+  defp do_create_student_grade_report_entry(_base_map, [], entries), do: entries
+
+  defp do_create_student_grade_report_entry(
+         %{} = base_map,
+         [
+           {grs_id, normalized_value, scale_value} | grsid_nv_sv_list
+         ],
+         entries
+       ) do
+    # setup student grade report entry attrs and create
+    case scale_value do
+      %OrdinalValue{} = ordinal_value ->
+        %{ordinal_value_id: ordinal_value.id}
+
+      score ->
+        %{score: score}
+    end
+    |> Enum.into(%{
+      normalized_value: normalized_value,
+      grades_report_subject_id: grs_id
+    })
+    |> Enum.into(base_map)
+    |> create_student_grade_report_entry()
+    |> case do
+      {:ok, student_grade_report_entry} ->
+        do_create_student_grade_report_entry(base_map, grsid_nv_sv_list, [
+          student_grade_report_entry | entries
+        ])
+
+      {:error, changeset} ->
+        Repo.rollback(changeset)
     end
   end
+
+  # helpers
 
   defp get_normalized_value_from_entry(%AssessmentPointEntry{scale_type: "ordinal"} = entry),
     do: entry.ordinal_value.normalized_value
--- test/lanttern/grades_reports_test.exs
+++ test/lanttern/grades_reports_test.exs
@@ -119,7 +119,7 @@ defmodule Lanttern.GradesReportsTest do
     alias Lanttern.SchoolsFixtures
     alias Lanttern.TaxonomyFixtures
 
-    test "calculate_student_grade/1 returns the correct student_grade_report_entry" do
+    test "calculate_student_grade/3 returns the correct student_grade_report_entry" do
       # marking scale
       # ordinal scale, 4 levels
       # 1 eme: 0.4
@@ -129,14 +129,14 @@ defmodule Lanttern.GradesReportsTest do
       #
       # grades scale
       # ordinal scale, 5 levels A, B, C, D, E (1.0, 0.85, 0.7, 0.5, 0)
-      # breakpoints: E - 0.4 - D - 0.6 - C - 0.8 - B - 0.9 - A
+      # breakpoints: E - 0.5 - D - 0.6 - C - 0.8 - B - 0.9 - A
       #
       # compositions: ap1 = w1, ap2 = w2, ap3 = w3, ap3_diff = w3
       #
       # test cases (in ap order)
       # eme - pro - ach = 0.69167 = C
       # ach - ach - exc = 0.92500 = A
-      # eme - eme - pro = 0.50000 = D (use diff in ap3)
+      # eme - pro - pro = 0.56667 = D (use diff in ap3)
 
       marking_scale = GradingFixtures.scale_fixture(%{type: "ordinal"})
 
@@ -156,7 +156,7 @@ defmodule Lanttern.GradesReportsTest do
         GradingFixtures.ordinal_value_fixture(%{scale_id: marking_scale.id, normalized_value: 1.0})
 
       grading_scale =
-        GradingFixtures.scale_fixture(%{type: "ordinal", breakpoints: [0.4, 0.6, 0.8, 0.9]})
+        GradingFixtures.scale_fixture(%{type: "ordinal", breakpoints: [0.5, 0.6, 0.8, 0.9]})
 
       ov_a =
         GradingFixtures.ordinal_value_fixture(%{scale_id: grading_scale.id, normalized_value: 1.0})
@@ -257,6 +257,36 @@ defmodule Lanttern.GradesReportsTest do
           weight: 3.0
         })
 
+      # extra fixtures for query test
+
+      other_goal =
+        AssessmentsFixtures.assessment_point_fixture(%{
+          strand_id: strand.id,
+          scale_id: marking_scale.id
+        })
+
+      other_subject = TaxonomyFixtures.subject_fixture()
+      other_cycle = SchoolsFixtures.cycle_fixture()
+
+      _other_grades_report_cycle =
+        ReportingFixtures.grades_report_cycle_fixture(%{
+          school_cycle_id: other_cycle.id,
+          grades_report_id: grades_report.id
+        })
+
+      _other_grades_report_subject =
+        ReportingFixtures.grades_report_subject_fixture(%{
+          subject_id: other_subject.id,
+          grades_report_id: grades_report.id
+        })
+
+      _other_grade_component =
+        ReportingFixtures.grade_component_fixture(%{
+          report_card_id: report_card.id,
+          subject_id: other_subject.id,
+          assessment_point_id: other_goal.id
+        })
+
       # case 1
       std_1 = SchoolsFixtures.student_fixture()
 
@@ -287,6 +317,17 @@ defmodule Lanttern.GradesReportsTest do
           ordinal_value_id: ov_ach.id
         })
 
+      # extra fixtures for query test
+
+      _other_entry =
+        AssessmentsFixtures.assessment_point_entry_fixture(%{
+          student_id: std_1.id,
+          assessment_point_id: other_goal.id,
+          scale_id: marking_scale.id,
+          scale_type: "ordinal",
+          ordinal_value_id: ov_ach.id
+        })
+
       expected_ov_id = ov_c.id
       expected_std_id = std_1.id
 
@@ -297,9 +338,9 @@ defmodule Lanttern.GradesReportsTest do
                 ordinal_value_id: ^expected_ov_id
               }} =
                GradesReports.calculate_student_grade(
-                 student_id: std_1.id,
-                 grades_report_cycle_id: grades_report_cycle.id,
-                 grades_report_subject_id: grades_report_subject.id
+                 std_1.id,
+                 grades_report_cycle.id,
+                 grades_report_subject.id
                )
 
       # case 2
@@ -342,9 +383,9 @@ defmodule Lanttern.GradesReportsTest do
                 ordinal_value_id: ^expected_ov_id
               }} =
                GradesReports.calculate_student_grade(
-                 student_id: std_2.id,
-                 grades_report_cycle_id: grades_report_cycle.id,
-                 grades_report_subject_id: grades_report_subject.id
+                 std_2.id,
+                 grades_report_cycle.id,
+                 grades_report_subject.id
                )
 
       # case 3 (diff)
@@ -365,7 +406,7 @@ defmodule Lanttern.GradesReportsTest do
           assessment_point_id: goal_2.id,
           scale_id: marking_scale.id,
           scale_type: "ordinal",
-          ordinal_value_id: ov_eme.id
+          ordinal_value_id: ov_pro.id
         })
 
       _entry_3_diff =
@@ -383,14 +424,354 @@ defmodule Lanttern.GradesReportsTest do
       assert {:ok,
               %StudentGradeReportEntry{
                 student_id: ^expected_std_id,
-                normalized_value: 0.5,
+                normalized_value: 0.56667,
                 ordinal_value_id: ^expected_ov_id
               }} =
                GradesReports.calculate_student_grade(
-                 student_id: std_3.id,
-                 grades_report_cycle_id: grades_report_cycle.id,
-                 grades_report_subject_id: grades_report_subject.id
+                 std_3.id,
+                 grades_report_cycle.id,
+                 grades_report_subject.id
                )
+    end
+
+    test "calculate_student_grades/2 returns the correct student grades report entries for given cycle" do
+      # marking scale
+      # ordinal scale, 4 levels
+      # 1 eme: 0.4
+      # 2 pro: 0.6
+      # 3 ach: 0.85
+      # 4 exc: 1.0
+      #
+      # grades scale
+      # ordinal scale, 5 levels A, B, C, D, E (1.0, 0.85, 0.7, 0.5, 0)
+      # breakpoints: E - 0.5 - D - 0.6 - C - 0.8 - B - 0.9 - A
+      #
+      # compositions (same for each strand): ap1 = w1, ap2 = w2, ap3 = w3
+      #
+      # test cases (in ap order)
+      # exc - ach - pro = 0.75000 = C
+      # eme - ach - exc = 0.85000 = B
+      # eme - eme - eme = 0.40000 = E
+
+      marking_scale = GradingFixtures.scale_fixture(%{type: "ordinal"})
+
+      ov_eme =
+        GradingFixtures.ordinal_value_fixture(%{scale_id: marking_scale.id, normalized_value: 0.4})
+
+      ov_pro =
+        GradingFixtures.ordinal_value_fixture(%{scale_id: marking_scale.id, normalized_value: 0.6})
+
+      ov_ach =
+        GradingFixtures.ordinal_value_fixture(%{
+          scale_id: marking_scale.id,
+          normalized_value: 0.85
+        })
+
+      ov_exc =
+        GradingFixtures.ordinal_value_fixture(%{scale_id: marking_scale.id, normalized_value: 1.0})
+
+      grading_scale =
+        GradingFixtures.scale_fixture(%{type: "ordinal", breakpoints: [0.5, 0.6, 0.8, 0.9]})
+
+      _ov_a =
+        GradingFixtures.ordinal_value_fixture(%{scale_id: grading_scale.id, normalized_value: 1.0})
+
+      ov_b =
+        GradingFixtures.ordinal_value_fixture(%{
+          scale_id: grading_scale.id,
+          normalized_value: 0.85
+        })
+
+      ov_c =
+        GradingFixtures.ordinal_value_fixture(%{scale_id: grading_scale.id, normalized_value: 0.7})
+
+      _ov_d =
+        GradingFixtures.ordinal_value_fixture(%{scale_id: grading_scale.id, normalized_value: 0.5})
+
+      ov_e =
+        GradingFixtures.ordinal_value_fixture(%{scale_id: grading_scale.id, normalized_value: 0.0})
+
+      strand_1 = LearningContextFixtures.strand_fixture()
+      strand_2 = LearningContextFixtures.strand_fixture()
+      strand_3 = LearningContextFixtures.strand_fixture()
+
+      goal_1_1 =
+        AssessmentsFixtures.assessment_point_fixture(%{
+          strand_id: strand_1.id,
+          scale_id: marking_scale.id
+        })
+
+      goal_1_2 =
+        AssessmentsFixtures.assessment_point_fixture(%{
+          strand_id: strand_1.id,
+          scale_id: marking_scale.id
+        })
+
+      goal_1_3 =
+        AssessmentsFixtures.assessment_point_fixture(%{
+          strand_id: strand_1.id,
+          scale_id: marking_scale.id
+        })
+
+      goal_2_1 =
+        AssessmentsFixtures.assessment_point_fixture(%{
+          strand_id: strand_2.id,
+          scale_id: marking_scale.id
+        })
+
+      goal_2_2 =
+        AssessmentsFixtures.assessment_point_fixture(%{
+          strand_id: strand_2.id,
+          scale_id: marking_scale.id
+        })
+
+      goal_2_3 =
+        AssessmentsFixtures.assessment_point_fixture(%{
+          strand_id: strand_2.id,
+          scale_id: marking_scale.id
+        })
+
+      goal_3_1 =
+        AssessmentsFixtures.assessment_point_fixture(%{
+          strand_id: strand_3.id,
+          scale_id: marking_scale.id
+        })
+
+      goal_3_2 =
+        AssessmentsFixtures.assessment_point_fixture(%{
+          strand_id: strand_3.id,
+          scale_id: marking_scale.id
+        })
+
+      goal_3_3 =
+        AssessmentsFixtures.assessment_point_fixture(%{
+          strand_id: strand_3.id,
+          scale_id: marking_scale.id
+        })
+
+      subject_1 = TaxonomyFixtures.subject_fixture()
+      subject_2 = TaxonomyFixtures.subject_fixture()
+      subject_3 = TaxonomyFixtures.subject_fixture()
+      cycle = SchoolsFixtures.cycle_fixture()
+      grades_report = ReportingFixtures.grades_report_fixture(%{scale_id: grading_scale.id})
+
+      grades_report_cycle =
+        ReportingFixtures.grades_report_cycle_fixture(%{
+          school_cycle_id: cycle.id,
+          grades_report_id: grades_report.id
+        })
+
+      grades_report_subject_1 =
+        ReportingFixtures.grades_report_subject_fixture(%{
+          subject_id: subject_1.id,
+          grades_report_id: grades_report.id
+        })
+
+      grades_report_subject_2 =
+        ReportingFixtures.grades_report_subject_fixture(%{
+          subject_id: subject_2.id,
+          grades_report_id: grades_report.id
+        })
+
+      grades_report_subject_3 =
+        ReportingFixtures.grades_report_subject_fixture(%{
+          subject_id: subject_3.id,
+          grades_report_id: grades_report.id
+        })
+
+      report_card =
+        ReportingFixtures.report_card_fixture(%{
+          school_cycle_id: cycle.id,
+          grades_report_id: grades_report.id
+        })
+
+      _grade_component_1_1 =
+        ReportingFixtures.grade_component_fixture(%{
+          report_card_id: report_card.id,
+          subject_id: subject_1.id,
+          assessment_point_id: goal_1_1.id,
+          weight: 1.0
+        })
+
+      _grade_component_1_2 =
+        ReportingFixtures.grade_component_fixture(%{
+          report_card_id: report_card.id,
+          subject_id: subject_1.id,
+          assessment_point_id: goal_1_2.id,
+          weight: 2.0
+        })
+
+      _grade_component_1_3 =
+        ReportingFixtures.grade_component_fixture(%{
+          report_card_id: report_card.id,
+          subject_id: subject_1.id,
+          assessment_point_id: goal_1_3.id,
+          weight: 3.0
+        })
+
+      _grade_component_2_1 =
+        ReportingFixtures.grade_component_fixture(%{
+          report_card_id: report_card.id,
+          subject_id: subject_2.id,
+          assessment_point_id: goal_2_1.id,
+          weight: 1.0
+        })
+
+      _grade_component_2_2 =
+        ReportingFixtures.grade_component_fixture(%{
+          report_card_id: report_card.id,
+          subject_id: subject_2.id,
+          assessment_point_id: goal_2_2.id,
+          weight: 2.0
+        })
+
+      _grade_component_2_3 =
+        ReportingFixtures.grade_component_fixture(%{
+          report_card_id: report_card.id,
+          subject_id: subject_2.id,
+          assessment_point_id: goal_2_3.id,
+          weight: 3.0
+        })
+
+      _grade_component_3_1 =
+        ReportingFixtures.grade_component_fixture(%{
+          report_card_id: report_card.id,
+          subject_id: subject_3.id,
+          assessment_point_id: goal_3_1.id,
+          weight: 1.0
+        })
+
+      _grade_component_3_2 =
+        ReportingFixtures.grade_component_fixture(%{
+          report_card_id: report_card.id,
+          subject_id: subject_3.id,
+          assessment_point_id: goal_3_2.id,
+          weight: 2.0
+        })
+
+      _grade_component_3_3 =
+        ReportingFixtures.grade_component_fixture(%{
+          report_card_id: report_card.id,
+          subject_id: subject_3.id,
+          assessment_point_id: goal_3_3.id,
+          weight: 3.0
+        })
+
+      std = SchoolsFixtures.student_fixture()
+
+      # subject 1
+
+      _entry_1_1 =
+        AssessmentsFixtures.assessment_point_entry_fixture(%{
+          student_id: std.id,
+          assessment_point_id: goal_1_1.id,
+          scale_id: marking_scale.id,
+          scale_type: "ordinal",
+          ordinal_value_id: ov_exc.id
+        })
+
+      _entry_1_2 =
+        AssessmentsFixtures.assessment_point_entry_fixture(%{
+          student_id: std.id,
+          assessment_point_id: goal_1_2.id,
+          scale_id: marking_scale.id,
+          scale_type: "ordinal",
+          ordinal_value_id: ov_ach.id
+        })
+
+      _entry_1_3 =
+        AssessmentsFixtures.assessment_point_entry_fixture(%{
+          student_id: std.id,
+          assessment_point_id: goal_1_3.id,
+          scale_id: marking_scale.id,
+          scale_type: "ordinal",
+          ordinal_value_id: ov_pro.id
+        })
+
+      # subject 2
+
+      _entry_2_1 =
+        AssessmentsFixtures.assessment_point_entry_fixture(%{
+          student_id: std.id,
+          assessment_point_id: goal_2_1.id,
+          scale_id: marking_scale.id,
+          scale_type: "ordinal",
+          ordinal_value_id: ov_eme.id
+        })
+
+      _entry_2_2 =
+        AssessmentsFixtures.assessment_point_entry_fixture(%{
+          student_id: std.id,
+          assessment_point_id: goal_2_2.id,
+          scale_id: marking_scale.id,
+          scale_type: "ordinal",
+          ordinal_value_id: ov_ach.id
+        })
+
+      _entry_2_3 =
+        AssessmentsFixtures.assessment_point_entry_fixture(%{
+          student_id: std.id,
+          assessment_point_id: goal_2_3.id,
+          scale_id: marking_scale.id,
+          scale_type: "ordinal",
+          ordinal_value_id: ov_exc.id
+        })
+
+      # subject 3
+
+      _entry_3_1 =
+        AssessmentsFixtures.assessment_point_entry_fixture(%{
+          student_id: std.id,
+          assessment_point_id: goal_3_1.id,
+          scale_id: marking_scale.id,
+          scale_type: "ordinal",
+          ordinal_value_id: ov_eme.id
+        })
+
+      _entry_3_2 =
+        AssessmentsFixtures.assessment_point_entry_fixture(%{
+          student_id: std.id,
+          assessment_point_id: goal_3_2.id,
+          scale_id: marking_scale.id,
+          scale_type: "ordinal",
+          ordinal_value_id: ov_eme.id
+        })
+
+      _entry_3_diff =
+        AssessmentsFixtures.assessment_point_entry_fixture(%{
+          student_id: std.id,
+          assessment_point_id: goal_3_3.id,
+          scale_id: marking_scale.id,
+          scale_type: "ordinal",
+          ordinal_value_id: ov_eme.id
+        })
+
+      assert {:ok, student_grade_report_entries} =
+               GradesReports.calculate_student_grades(
+                 std.id,
+                 grades_report_cycle.id
+               )
+
+      assert Enum.find(
+               student_grade_report_entries,
+               &(&1.student_id == std.id and &1.normalized_value == 0.75 and
+                   &1.ordinal_value_id == ov_c.id and
+                   &1.grades_report_subject_id == grades_report_subject_1.id)
+             )
+
+      assert Enum.find(
+               student_grade_report_entries,
+               &(&1.student_id == std.id and &1.normalized_value == 0.85 and
+                   &1.ordinal_value_id == ov_b.id and
+                   &1.grades_report_subject_id == grades_report_subject_2.id)
+             )
+
+      assert Enum.find(
+               student_grade_report_entries,
+               &(&1.student_id == std.id and &1.normalized_value == 0.4 and
+                   &1.ordinal_value_id == ov_e.id and
+                   &1.grades_report_subject_id == grades_report_subject_3.id)
+             )
     end
   end
 end
