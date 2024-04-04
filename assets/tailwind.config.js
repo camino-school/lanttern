84536f4efa58288e6fa97594e96651766071a846
endoooo <eric.ehe@gmail.com>
2024-04-04 15:21:54 -0300

    feat: added support to grades info in student report card
    - added prose headings (h1, h2, etc.) styles (using Montserrat)

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -42,6 +42,18 @@ module.exports = {
       aria: {
         current: 'current="true"'
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            h1: { fontFamily: 'Montserrat, sans-serif', fontWeight: '900' },
+            h2: { fontFamily: 'Montserrat, sans-serif', fontWeight: '900' },
+            h3: { fontFamily: 'Montserrat, sans-serif', fontWeight: '700' },
+            h4: { fontFamily: 'Montserrat, sans-serif', fontWeight: '700' },
+            h5: { fontFamily: 'Montserrat, sans-serif' },
+            h6: { fontFamily: 'Montserrat, sans-serif' },
+          }
+        }
+      })
     },
   },
   plugins: [
