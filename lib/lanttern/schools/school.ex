fbb6870749a80b1bddcc2ade665a971bfd335d6d
endoooo <eric.ehe@gmail.com>
2024-04-11 03:28:11 -0300

    feat: added support to school branding
    - added `logo_image_url`, `bg_color`, and `text_color` fields to `School` schema
    
    - added school branding UI to `StudentHomeLive`

--- lib/lanttern/schools/school.ex
+++ lib/lanttern/schools/school.ex
@@ -2,15 +2,23 @@ defmodule Lanttern.Schools.School do
   use Ecto.Schema
   import Ecto.Changeset
 
+  import LantternWeb.Gettext
+
   @type t :: %__MODULE__{
           id: pos_integer(),
           name: String.t(),
+          logo_image_url: String.t(),
+          bg_color: String.t(),
+          text_color: String.t(),
           inserted_at: DateTime.t(),
           updated_at: DateTime.t()
         }
 
   schema "schools" do
     field :name, :string
+    field :logo_image_url, :string
+    field :bg_color, :string
+    field :text_color, :string
 
     timestamps()
   end
@@ -18,7 +26,15 @@ defmodule Lanttern.Schools.School do
   @doc false
   def changeset(school, attrs) do
     school
-    |> cast(attrs, [:name])
+    |> cast(attrs, [:name, :logo_image_url, :bg_color, :text_color])
     |> validate_required([:name])
+    |> check_constraint(:bg_color,
+      name: :school_bg_color_should_be_hex,
+      message: gettext("Invalid hex color")
+    )
+    |> check_constraint(:text_color,
+      name: :school_text_color_should_be_hex,
+      message: gettext("Invalid hex color")
+    )
   end
 end
--- lib/lanttern_web/live/pages/student/student_home_live.ex
+++ lib/lanttern_web/live/pages/student/student_home_live.ex
@@ -6,6 +6,8 @@ defmodule LantternWeb.StudentHomeLive do
   use LantternWeb, :live_view
 
   alias Lanttern.Reporting
+  alias Lanttern.Schools
+  import LantternWeb.SupabaseHelpers, only: [object_url_to_render_url: 2]
 
   # shared components
   import LantternWeb.ReportingComponents
@@ -20,10 +22,29 @@ defmodule LantternWeb.StudentHomeLive do
 
     has_student_report_cards = length(student_report_cards) > 0
 
+    school =
+      socket.assigns.current_user.current_profile.school_id
+      |> Schools.get_school!()
+
+    logo_image_url =
+      case school.logo_image_url do
+        nil ->
+          nil
+
+        url ->
+          object_url_to_render_url(
+            url,
+            width: 128,
+            height: 128
+          )
+      end
+
     socket =
       socket
       |> stream(:student_report_cards, student_report_cards)
       |> assign(:has_student_report_cards, has_student_report_cards)
+      |> assign(:school, school)
+      |> assign(:logo_image_url, logo_image_url)
 
     {:ok, socket}
   end
