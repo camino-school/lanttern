8c9eb2bffc5e32588f7e8e8c347256a2685cd37b
endoooo <eric.ehe@gmail.com>
2024-04-25 11:32:27 -0300

    feat: added separated report cards list to student and guardian home pages
    - added support to `is_wip` and `hide_description` attrs in `<.report_card_card>`
    - added support to report card description in `<.report_card_card>` and in student report card detail page

--- lib/lanttern_web/components/reporting_components.ex
+++ lib/lanttern_web/components/reporting_components.ex
@@ -23,7 +23,9 @@ defmodule LantternWeb.ReportingComponents do
   attr :report_card, ReportCard, required: true
   attr :cycle, Cycle, default: nil
   attr :year, Year, default: nil
+  attr :is_wip, :boolean, default: false
   attr :navigate, :string, default: nil
+  attr :hide_description, :boolean, default: false
   attr :id, :string, default: nil
   attr :class, :any, default: nil
 
@@ -51,12 +53,13 @@ defmodule LantternWeb.ReportingComponents do
           "font-display font-black text-2xl line-clamp-3",
           "md:text-3xl"
         ]}>
-          <%= if @navigate do %>
+          <%= if @navigate && not @is_wip do %>
             <.link navigate={@navigate} class="underline hover:text-ltrn-subtle">
               <%= @report_card.name %>
             </.link>
           <% else %>
-            <%= @report_card.name %>
+            <span :if={@is_wip} class="text-ltrn-subtle"><%= @report_card.name %></span>
+            <%= if !@is_wip, do: @report_card.name %>
           <% end %>
         </h5>
         <div :if={@cycle || @report_card.year} class="flex flex-wrap gap-2">
@@ -67,6 +70,13 @@ defmodule LantternWeb.ReportingComponents do
             <%= @year.name %>
           </.badge>
         </div>
+        <div :if={!@hide_description && @report_card.description} class="line-clamp-3">
+          <.markdown text={@report_card.description} size="sm" />
+        </div>
+      </div>
+      <div :if={@is_wip} class="flex items-center gap-2 p-4 text-sm text-ltrn-subtle bg-ltrn-lightest">
+        <.icon name="hero-lock-closed-mini" />
+        <%= gettext("Under development") %>
       </div>
     </div>
     """
--- lib/lanttern_web/live/pages/guardian/guardian_home_live.ex
+++ lib/lanttern_web/live/pages/guardian/guardian_home_live.ex
@@ -14,13 +14,23 @@ defmodule LantternWeb.GuardianHomeLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    student_report_cards =
+    all_student_report_cards =
       Reporting.list_student_report_cards(
         student_id: socket.assigns.current_user.current_profile.guardian_of_student_id,
         preloads: [report_card: [:year, :school_cycle]]
       )
 
+    student_report_cards =
+      all_student_report_cards
+      |> Enum.filter(& &1.allow_guardian_access)
+
     has_student_report_cards = length(student_report_cards) > 0
+
+    student_report_cards_wip =
+      all_student_report_cards
+      |> Enum.filter(&(not &1.allow_guardian_access))
+
+    has_student_report_cards_wip = length(student_report_cards_wip) > 0
 
     school =
       socket.assigns.current_user.current_profile.school_id
@@ -30,6 +40,8 @@ defmodule LantternWeb.GuardianHomeLive do
       socket
       |> stream(:student_report_cards, student_report_cards)
       |> assign(:has_student_report_cards, has_student_report_cards)
+      |> stream(:student_report_cards_wip, student_report_cards_wip)
+      |> assign(:has_student_report_cards_wip, has_student_report_cards_wip)
       |> assign(:school, school)
 
     {:ok, socket}
