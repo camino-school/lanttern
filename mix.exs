0a8855411950c47976e53b550647251ad2e9bf2f
endoooo <eric.ehe@gmail.com>
2024-05-02 15:09:20 -0300

    chore: updated version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lanttern.MixProject do
   def project do
     [
       app: :lanttern,
-      version: "2024.4.30-alpha.15",
+      version: "2024.5.2-alpha.16",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
