c4146997033eb0406a583b1cf409b074f027f51e
endoooo <eric.ehe@gmail.com>
2024-06-20 08:41:49 -0300

    chore: updated translation and versions

--- lib/lanttern_web/live/pages/report_cards/id/students_grades_component.ex
+++ lib/lanttern_web/live/pages/report_cards/id/students_grades_component.ex
@@ -431,8 +431,8 @@ defmodule LantternWeb.ReportCardLive.StudentsGradesComponent do
   defp build_calculation_results_message([{:updated_with_manual, count} | results], msgs) do
     msg =
       ngettext(
-        "1 grade composition updated (manual grade not changed)",
-        "%{count} grades compositions updated (manual grades not changed)",
+        "1 grade partially updated (only composition, manual grade not changed)",
+        "%{count} grades partially updated (only compositions, manual grades not changed)",
         count
       )
 
