a3063b013ee72a4e1bf307e5513d84182f635042
PitzTech <victorlaurentino7@gmail.com>
2026-02-24 21:37:14 -0300

    refactor the test to use phoenix_test + factories.

--- test/lanttern_web/live/pages/school/classes/id/class_live_test.exs
+++ test/lanttern_web/live/pages/school/classes/id/class_live_test.exs
@@ -1,7 +1,12 @@
 defmodule LantternWeb.ClassLiveTest do
   use LantternWeb.ConnCase
 
-  alias Lanttern.SchoolsFixtures
+  import Lanttern.Factory
+  import PhoenixTest
+
+  alias Lanttern.Repo
+  alias Lanttern.Schools
+  alias Lanttern.Schools.School
 
   @live_view_base_path "/school/classes"
 
@@ -9,15 +14,10 @@ defmodule LantternWeb.ClassLiveTest do
 
   describe "Class live view basic navigation" do
     test "disconnected and connected mount", %{conn: conn, user: user} do
-      school_id = user.current_profile.school_id
+      school = Repo.get!(School, user.current_profile.school_id)
       cycle = user.current_profile.current_school_cycle
 
-      class =
-        SchoolsFixtures.class_fixture(%{
-          school_id: school_id,
-          cycle_id: cycle.id,
-          name: "some class abc xyz"
-        })
+      class = insert(:class, school: school, cycle: cycle, name: "some class abc xyz")
 
       conn = get(conn, "#{@live_view_base_path}/#{class.id}/people")
 
@@ -27,92 +27,80 @@ defmodule LantternWeb.ClassLiveTest do
     end
 
     test "list students", %{conn: conn, user: user} do
-      school_id = user.current_profile.school_id
-      cycle_id = user.current_profile.current_school_cycle.id
+      school = Repo.get!(School, user.current_profile.school_id)
+      cycle = user.current_profile.current_school_cycle
 
-      class =
-        SchoolsFixtures.class_fixture(%{school_id: school_id, cycle_id: cycle_id})
+      class = insert(:class, school: school, cycle: cycle)
 
-      student_a =
-        SchoolsFixtures.student_fixture(%{
-          name: "student a",
-          school_id: school_id,
-          classes_ids: [class.id]
-        })
+      student_a = insert(:student, school_id: school.id, name: "student a")
+      student_a = Repo.preload(student_a, :classes)
+      {:ok, student_a} = Schools.update_student(student_a, %{classes_ids: [class.id]})
 
-      student_b =
-        SchoolsFixtures.student_fixture(%{
-          name: "student b",
-          school_id: school_id,
-          classes_ids: [class.id]
-        })
+      student_b = insert(:student, school_id: school.id, name: "student b")
+      student_b = Repo.preload(student_b, :classes)
+      {:ok, student_b} = Schools.update_student(student_b, %{classes_ids: [class.id]})
 
-      {:ok, view, _html} = live(conn, "#{@live_view_base_path}/#{class.id}/people")
-
-      assert view |> has_element?("a", student_a.name)
-      assert view |> has_element?("a", student_b.name)
-
-      view
-      |> element("a", student_a.name)
-      |> render_click()
-
-      assert_redirect(view, ~p"/school/students/#{student_a}")
+      conn
+      |> visit("#{@live_view_base_path}/#{class.id}/people")
+      |> assert_has("a", text: student_a.name)
+      |> assert_has("a", text: student_b.name)
+      |> click_link(student_a.name)
+      |> assert_path(~p"/school/students/#{student_a}")
     end
 
     test "displays both staff and students sections in people tab", %{conn: conn, user: user} do
-      school_id = user.current_profile.school_id
-      cycle_id = user.current_profile.current_school_cycle.id
+      school = Repo.get!(School, user.current_profile.school_id)
+      cycle = user.current_profile.current_school_cycle
 
-      class = SchoolsFixtures.class_fixture(%{school_id: school_id, cycle_id: cycle_id})
+      class = insert(:class, school: school, cycle: cycle)
 
-      student =
-        SchoolsFixtures.student_fixture(%{
-          school_id: school_id,
-          classes_ids: [class.id]
-        })
+      student = insert(:student, school_id: school.id)
+      student = Repo.preload(student, :classes)
+      {:ok, student} = Schools.update_student(student, %{classes_ids: [class.id]})
 
-      {:ok, view, _html} = live(conn, "#{@live_view_base_path}/#{class.id}/people")
-
-      assert view |> has_element?("#staff-section")
-      assert view |> has_element?("#students-section")
-      assert view |> has_element?("a", student.name)
+      conn
+      |> visit("#{@live_view_base_path}/#{class.id}/people")
+      |> assert_has("#staff-section")
+      |> assert_has("#students-section")
+      |> assert_has("a", text: student.name)
     end
 
     test "sections are expanded by default", %{conn: conn, user: user} do
-      school_id = user.current_profile.school_id
-      cycle_id = user.current_profile.current_school_cycle.id
+      school = Repo.get!(School, user.current_profile.school_id)
+      cycle = user.current_profile.current_school_cycle
 
-      class = SchoolsFixtures.class_fixture(%{school_id: school_id, cycle_id: cycle_id})
+      class = insert(:class, school: school, cycle: cycle)
 
-      {:ok, view, _html} = live(conn, "#{@live_view_base_path}/#{class.id}/people")
-
-      # Check that content divs are visible (not hidden)
-      assert view |> has_element?("#staff-section-content:not(.hidden)")
-      assert view |> has_element?("#students-section-content:not(.hidden)")
+      conn
+      |> visit("#{@live_view_base_path}/#{class.id}/people")
+      |> assert_has("#staff-section-content:not(.hidden)")
+      |> assert_has("#students-section-content:not(.hidden)")
     end
   end
 
   describe "Class management permissions" do
     test "allow user with school management permissions to edit class", context do
       %{conn: conn, user: user} = set_user_permissions(["school_management"], context)
-      school_id = user.current_profile.school_id
-      class = SchoolsFixtures.class_fixture(%{school_id: school_id, name: "class abc"})
+      school = Repo.get!(School, user.current_profile.school_id)
 
-      {:ok, view, _html} = live(conn, "#{@live_view_base_path}/#{class.id}/people?edit=true")
+      class = insert(:class, school: school, name: "class abc")
 
-      assert view |> has_element?("#class-form-overlay h2", "Edit class")
+      conn
+      |> visit("#{@live_view_base_path}/#{class.id}/people?edit=true")
+      |> assert_has("#class-form-overlay h2", text: "Edit class")
     end
 
     test "prevent user without school management permissions to edit staff member", %{
       conn: conn,
       user: user
     } do
-      school_id = user.current_profile.school_id
-      class = SchoolsFixtures.class_fixture(%{school_id: school_id, name: "class abc"})
+      school = Repo.get!(School, user.current_profile.school_id)
 
-      {:ok, view, _html} = live(conn, "#{@live_view_base_path}/#{class.id}/people?edit=true")
+      class = insert(:class, school: school, name: "class abc")
 
-      refute view |> has_element?("#class-form-overlay h2", "Edit class")
+      conn
+      |> visit("#{@live_view_base_path}/#{class.id}/people?edit=true")
+      |> refute_has("#class-form-overlay h2", text: "Edit class")
     end
   end
 end
