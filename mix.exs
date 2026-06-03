08356ed905a03d17efeae5a48bf3ec89e2530d86
Eric Endo <eric.endo@caminoschool.com.br>
2026-06-02 21:25:28 -0300

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lanttern.MixProject do
   def project do
     [
       app: :lanttern,
-      version: "2026.5.25-alpha.97",
+      version: "2026.6.2-alpha.98",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
